37e7f33b909c5d0d16d1bc748292d08292f2f86c
Rahul <68159064+RahulVashishtha1@users.noreply.github.com>
2024-04-02 22:57:00 +0530

    handled Employee class to avoid prompts for dummy data

--- Employee.java
+++ Employee.java
@@ -11,16 +11,13 @@ public class Employee {
     int salary;
     String type;
 
-    
-    public Employee() throws IOException
-    {
+    public Employee() throws IOException {
         BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.print("\nEmployee's Name: ");
         // employeeName = scan.nextLine();
         employeeName = bfr.readLine();
-  
 
         System.out.print("Employee ID: ");
         employeeID = scan.nextInt();
@@ -29,22 +26,29 @@ public class Employee {
         // department = scan.nextLine();
         department = bfr.readLine();
 
-
         System.out.print("Job Title: ");
         // jobTitle = scan.nextLine();
         jobTitle = bfr.readLine();
 
-            
         System.out.print("Salary: ");
         salary = scan.nextInt();
     }
 
-    public void displayEmployee()
-    {
+    // dummy data constructor
+    public Employee(String employeeName, int employeeID, String department, String jobTitle, int salary, String type) {
+        this.employeeName = employeeName;
+        this.employeeID = employeeID;
+        this.department = department;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+        this.type = type;
+    }
+
+    public void displayEmployee() {
         System.out.println("FULL NAME: " + employeeName);
         System.out.println("Employee ID: " + employeeID);
         System.out.println("Department: " + department);
-        System.out.println("Job Title: "  + jobTitle);
+        System.out.println("Job Title: " + jobTitle);
         System.out.println("Salary: " + salary);
     }
 }
